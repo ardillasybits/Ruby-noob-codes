68a6fefaba22c709aca06e86f8dce9f40621f0e1
Git Master <gitmaster@ardillasybits.com>
2011-06-07 01:14:51 -0500

    adding comments

--- tp/TP2010-1PC2-2.rb
+++ tp/TP2010-1PC2-2.rb
@@ -1,25 +1,24 @@
 class Numeric
-  def degrees
-    self * Math::PI / 180 # so .degrees will conver the number to radians
-  end
+	def degrees
+		self * Math::PI / 180 # so .degrees will conver the number to radians
+	end
 end
-
 
 class TiroBlanco
 
-def Xposition(velocidad, angulo, tiempo)
-	x = velocidad * tiempo * Math.cos(angulo.degrees)
-	return x
+	def Xposition(velocidad, angulo, tiempo)
+		x = velocidad * tiempo * Math.cos(angulo.degrees)
+		return x
+	end
+
+	def Yposition(velocidad, angulo, tiempo)
+		y = (velocidad * Math.sin(angulo.degrees) * tiempo) - (0.5 * 9.8 * (tiempo**2))
+		return y
+	end
+
 end
 
-def Yposition(velocidad, angulo, tiempo)
-	y = (velocidad * Math.sin(angulo.degrees) * tiempo) - (0.5 * 9.8 * (tiempo**2))
-	return y
-end
-
-end
-
-LONGITUD = 10
+LONGITUD = 10 #constant
 
 coordenadas = TiroBlanco.new
 
@@ -40,15 +39,16 @@ zona_caida = altura/2
 tiempo_inicial = 0
 posicionX = 0
 longitudX = 0
+
 until longitudX >= LONGITUD
 
 posicionX = coordenadas.Xposition(velocidad_inicial, angulo_inicial, tiempo_inicial)
 posicionY = coordenadas.Yposition(velocidad_inicial, angulo_inicial, tiempo_inicial)
 
 if posicionY <= 0 then
-velocidad_inicial = velocidad_inicial * 0.85
-angulo_inicial = angulo_inicial * 0.85
-posicionY = 0
+	velocidad_inicial = velocidad_inicial * 0.85
+	angulo_inicial = angulo_inicial * 0.85
+	posicionY = 0 #we reset Y position because it can't be underground...
 end
 
 puts "#######"
@@ -60,6 +60,6 @@ puts "Posicion Y : "
 puts posicionY.to_f
 puts "#######"
 
-longitudX = posicionX
+longitudX = posicionX #finish the until here because we want all messages to appear!
 tiempo_inicial += intervalos
 end
